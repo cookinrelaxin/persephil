562a1ee57d6178d28e916fae9544538ba65b5103
Zachary Feldcamp <zachary.feldcamp@me.com>
2021-01-15 22:30:08 -0700

    Switched to xlsx.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const got = require('got');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
-const csv = require('csv');
+const excelJS = require('excelJS');
 const moment = require('moment');
 
 const url = process.argv.slice(2)[0]
@@ -13,30 +13,37 @@ got(url).then(response => {
 	
 	const kwicRows = document.querySelectorAll('.content');
 
-	var entries = [];
+	const workbook = new excelJS.Workbook();
+	const worksheet = workbook.addWorksheet('Data');
+	worksheet.columns = [
+		{ header: 'Text', key: 'text', width: 75, style: { alignment: { wrapText: true } } },
+		{ header: 'Work', key: 'work', width: 15 },
+		{ header: 'Passage', key: 'passage', width: 15}
+	];
 
-	for (const kwicRow of kwicRows) {
+ 	for (const kwicRow of kwicRows) {
 		const text = kwicRow.textContent
 		const title = kwicRow.nextSibling;
 		const work = title.querySelector('b');
 		const passage = title.querySelector('a');
-		
-		const entry = 	{	'work': 		work.textContent.trim(),
-					'passage': 	passage.textContent.trim(),
-					'text':		text.trim()
-				};
-		entries.push(entry);
-	}	
-	serialized = csv.stringify(	entries, 
-					{ 	columns: [ { key: 'text' }, {key: 'work'}, {key: 'passage'} ],
-						delimiter: '|' 
-					});
-	serialized.pipe(fs.createWriteStream(`Latin corpus search ${moment().format('MM-DD-YY HH mm ss')}.csv`));
+
+		worksheet.addRow({	text: text.trim(),
+					work: work.textContent.trim(),
+					passage: passage.textContent.trim()
+				});
+	}
+
+	worksheet.getRow(1).font = { bold: true };
+	
+	filename = `Latin corpus search ${moment().format('MM-DD-YY HH mm ss')}.xlsx`;
+
+	workbook.xlsx.writeFile(filename).then(() => {
+		console.log(filename);
+	}).catch((err) => {
+		console.log(err);
+	});
+	// serialized.pipe(fs.createWriteStream(filename, {mode: '0444'}));
 
 }).catch(err => {
 	console.log(err);
 });
-
-function kwicText(span) {
-	return;
-};
